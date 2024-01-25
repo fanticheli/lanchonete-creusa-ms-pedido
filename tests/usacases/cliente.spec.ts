ead77ecea1081310ef3d5a32e999b7c0959fa9a0
LucasSCruz <lucasscruz2001@gmai.com>
2024-01-25 12:18:23 -0300

    Adicionando novos testes

--- tests/usacases/cliente.spec.ts
+++ tests/usacases/cliente.spec.ts
@@ -1,9 +1,8 @@
-import { ClienteProps } from "../../src/entities/props/cliente.props"
-import { ClienteRepositoryInMemory } from "../../src/external/memory/cliente.repository"
-import { ClienteUseCases } from "../../src/usecases/cliente"
+import { ClienteProps } from "../../src/entities/props/cliente.props";
+import { ClienteRepositoryInMemory } from "../../src/external/memory/cliente.repository";
+import { ClienteUseCases } from "../../src/usecases/cliente";
 
 describe('Cliente usecases', () => {
-
     const clienteRepository = new ClienteRepositoryInMemory();
 
     test('should create a new client', async () => {
@@ -13,26 +12,51 @@ describe('Cliente usecases', () => {
             email: 'joão@joão.com.br',
             cpf: '360.635.210-70'
         }
-        const novoCliente = await ClienteUseCases.CriarCliente(clienteRepository, clienteProps)
+        const novoCliente = await ClienteUseCases.CriarCliente(clienteRepository, clienteProps);
         const clientes = await ClienteUseCases.BuscarTodosClientes(clienteRepository);
 
-        expect(novoCliente).toBeDefined()
-        expect(novoCliente?.id).toBe('1')
-        expect(novoCliente?.nome).toBe('João')
-        expect(novoCliente?.email).toBe('joão@joão.com.br')
-        expect(novoCliente?.cpf).toBe('36063521070')
-        expect(clientes).toBeDefined()
-        expect(clientes).toHaveLength(1)
-    })
+        expect(novoCliente).toBeDefined();
+        expect(novoCliente?.id).toBe('1');
+        expect(novoCliente?.nome).toBe('João');
+        expect(novoCliente?.email).toBe('joão@joão.com.br');
+        expect(novoCliente?.cpf).toBe('36063521070');
+        expect(clientes).toBeDefined();
+        expect(clientes).toHaveLength(1);
+    });
 
     test('should find a client by CPF', async () => {
         const cliente = await ClienteUseCases.BuscarClientePorCPF(clienteRepository, '36063521070');
         
-        expect(cliente).toBeDefined()
-        expect(cliente?.id).toBe('1')
-        expect(cliente?.nome).toBe('João')
-        expect(cliente?.email).toBe('joão@joão.com.br')
-        expect(cliente?.cpf).toBe('36063521070')
-    })
+        expect(cliente).toBeDefined();
+        expect(cliente?.id).toBe('1');
+        expect(cliente?.nome).toBe('João');
+        expect(cliente?.email).toBe('joão@joão.com.br');
+        expect(cliente?.cpf).toBe('36063521070');
+    });
+});
 
-})
+describe('BuscarTodosClientes usecase', () => {
+    const clienteRepository = new ClienteRepositoryInMemory();
+
+    test('should return all clients', async () => {
+        await ClienteUseCases.CriarCliente(clienteRepository, {
+            id: '2',
+            nome: 'Maria',
+            email: 'maria@maria.com.br',
+            cpf: '321.466.799-01'
+        });
+
+        const clientes = await ClienteUseCases.BuscarTodosClientes(clienteRepository);
+
+        expect(clientes).toBeDefined();
+        expect(clientes).toHaveLength(2); 
+    });
+
+    test('should return null if no clients are found', async () => {
+        clienteRepository.clear();
+
+        const clientes = await ClienteUseCases.BuscarTodosClientes(clienteRepository);
+
+        expect(clientes).toBeNull();
+    });
+});
--- tests/usacases/pedido.spec.ts
+++ tests/usacases/pedido.spec.ts
@@ -57,5 +57,60 @@ describe("Pedido", () => {
 		expect(novoPedido.statusPagamento).toBe(StatusPagamentoEnum.PENDENTE);
 		expect(novoPedido.statusPedido).toBe(StatusPedidoEnum.RECEBIDO);
 	});
+	
+	
+	test('Deve criar um pagamento', async () => {
+		const mockResponse = {
+		  data: {
+			codigoPix: '987678954',
+		  },
+		};
+	
+		axios.post.mockImplementation(() => Promise.resolve(mockResponse));
+	
+		const pedidoProps: PedidoProps = {
+		  produtos: ['1'],
+		  cliente: 'Cliente 1',
+		  valorTotal: 0,
+		  numeroPedido: 0,
+		  statusPagamento: StatusPagamentoEnum.PENDENTE,
+		  statusPedido: StatusPedidoEnum.RECEBIDO,
+		};
+	
+		const novoPagamento = await PedidoUseCases.CriarPagamento(pedidoProps);
+
+		expect(novoPagamento).toBeDefined();
+	  });
+
+	  test('Deve alterar o status do pagamento do pedido', async () => {
+		const pedidoEncontradoMock: PedidoOutput = {
+		  id: '1',
+		  statusPagamento: StatusPagamentoEnum.PENDENTE,
+		  statusPedido: StatusPedidoEnum.RECEBIDO,
+		};
+	
+		const codigoPagamento = '123456789';
+		const statusPagamento = StatusPagamentoEnum.APROVADO;
+
+		pedidoRepository.BuscarPedidoPorCodigoPagamento = jest.fn().mockResolvedValue(pedidoEncontradoMock);
+	
+		const updatedPedido = await PedidoUseCases.AlterarStatusPagamentoPedido(
+		  pedidoRepository,
+		  codigoPagamento,
+		  statusPagamento
+		);
+	
+		expect(pedidoRepository.BuscarPedidoPorCodigoPagamento).toHaveBeenCalledWith(codigoPagamento);
+		expect(pedidoRepository.EditarPedido).toHaveBeenCalledWith(expect.any(Object));
+	
+		expect(updatedPedido).toBeDefined();
+		expect(updatedPedido.statusPagamento).toBe(statusPagamento);
+	
+		if (statusPagamento === StatusPagamentoEnum.NEGADO) {
+		  expect(updatedPedido.statusPedido).toBe(StatusPedidoEnum.CANCELADO);
+		} else if (statusPagamento === StatusPagamentoEnum.APROVADO) {
+		  expect(updatedPedido.statusPedido).toBe(StatusPedidoEnum.PREPARACAO);
+		}
+	  });
 
 });
