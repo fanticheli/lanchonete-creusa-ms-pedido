c1feee75e249d0416104c8678d139bfb58b1127c
fanticheli <igor.fanticheli@hotmail.com>
2024-01-25 16:29:07 -0300

    fix: security sonar

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,10 +4,10 @@ export class LanchoneteCreusa {
 	start() {
 		const express = require("express");
 
-		const app = express();
+		const lanchoneteCreusa = express();
 		const swaggerUi = require("swagger-ui-express");
 		const swaggerSpec = require("./swagger");
-		app.use(express.json());
+		lanchoneteCreusa.use(express.json());
 		const PORT = process.env.PORT || 9000;
 
 		const docsRoutes = require("./routes/docs");
@@ -16,14 +16,14 @@ export class LanchoneteCreusa {
 		const produtosRoutes = require("./routes/produto");
 		const pedidosRoutes = require("./routes/pedido");
 
-		app.use("/api", indexRoutes);
-		app.use("/api-json", docsRoutes);
-		app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-		app.use("/api/clientes", clienteRoutes);
-		app.use("/api/produtos", produtosRoutes);
-		app.use("/api/pedidos", pedidosRoutes);
+		lanchoneteCreusa.use("/api", indexRoutes);
+		lanchoneteCreusa.use("/api-json", docsRoutes);
+		lanchoneteCreusa.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+		lanchoneteCreusa.use("/api/clientes", clienteRoutes);
+		lanchoneteCreusa.use("/api/produtos", produtosRoutes);
+		lanchoneteCreusa.use("/api/pedidos", pedidosRoutes);
 
-		app.listen(PORT, () => {
+		lanchoneteCreusa.listen(PORT, () => {
 			console.log(`Lanchonete da Creusa app listening on port ${PORT}`);
 		});
 	}
