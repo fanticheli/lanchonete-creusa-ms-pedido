b504077499cab00e78de0c3fe3b8ce4d0897297c
LucasSCruz <lucasscruz2001@gmai.com>
2024-01-25 15:51:05 -0300

    Ajuste no pedido.spec.ts

--- tests/usacases/pedido.spec.ts
+++ tests/usacases/pedido.spec.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { CategoriaEnum } from "../../src/common/enum/categoria-enum";
 import { StatusPagamentoEnum } from "../../src/common/enum/status-pagamento-enum";
 import { StatusPedidoEnum } from "../../src/common/enum/status-pedido-enum";
@@ -9,110 +9,50 @@ import { ProdutoRepositoryInMemory } from "../../src/external/memory/produto.rep
 import { PedidoUseCases } from "../../src/usecases/pedido";
 import { ProdutoUseCases } from "../../src/usecases/produtos";
 
-jest.mock('axios');
+jest.unmock('axios'); 
 
 describe("Pedido", () => {
-	const produtoRepository = new ProdutoRepositoryInMemory();
-	const pedidoRepository = new PedidoRepositoryInMemory();
+  const produtoRepository = new ProdutoRepositoryInMemory();
+  const pedidoRepository = new PedidoRepositoryInMemory();
 
-	test("Deve criar um pedido", async () => {
-		const mockResponse = {
-			data: {
-				codigoPix: "123456789",
-			},
-		};
+  beforeEach(() => {
+    jest.clearAllMocks(); 
+  });
 
-		axios.post.mockImplementation(() => Promise.resolve(mockResponse));
+  test('Deve alterar o status do pagamento do pedido', async () => {
+    const pedidoEncontradoMock: PedidoOutput = {
+      id: '1',
+      statusPagamento: StatusPagamentoEnum.PENDENTE,
+      statusPedido: StatusPedidoEnum.RECEBIDO,
+    };
 
-		const produtoProps: ProdutoProps = {
-			id: "1",
-			descricao: "Produto 1",
-			valor: 10,
-			categoria: CategoriaEnum.LANCHE,
-		};
+    const codigoPagamento = '123456789';
+    const statusPagamento = StatusPagamentoEnum.APROVADO;
 
-		await ProdutoUseCases.CriarProduto(produtoRepository, produtoProps);
+    pedidoRepository.BuscarPedidoPorCodigoPagamento = jest.fn().mockResolvedValue(pedidoEncontradoMock);
 
-		const pedidoProps: PedidoProps = {
-			produtos: ["1"],
-			cliente: "Cliente 1",
-			valorTotal: 0,
-			numeroPedido: 0,
-			statusPagamento: StatusPagamentoEnum.PENDENTE,
-			statusPedido: StatusPedidoEnum.RECEBIDO,
-		};
+    const updatedPedido = await PedidoUseCases.AlterarStatusPagamentoPedido(
+      pedidoRepository,
+      codigoPagamento,
+      statusPagamento
+    );
 
-		const novoPedido = await PedidoUseCases.CriarPedido(
-			pedidoRepository,
-			produtoRepository,
-			pedidoProps
-		);
+    expect(pedidoRepository.BuscarPedidoPorCodigoPagamento).toHaveBeenCalledWith(codigoPagamento);
 
-		expect(novoPedido).toBeDefined();
-		expect(novoPedido.id).toBe("01");
-		expect(novoPedido.produtos).toHaveLength(1);
-		expect(novoPedido.valorTotal).toBe(10);
-		expect(novoPedido.numeroPedido).toBe(1);
-		expect(novoPedido.cliente).toBe("Cliente 1");
-		expect(novoPedido.statusPagamento).toBe(StatusPagamentoEnum.PENDENTE);
-		expect(novoPedido.statusPedido).toBe(StatusPedidoEnum.RECEBIDO);
-	});
-	
-	
-	test('Deve criar um pagamento', async () => {
-		const mockResponse = {
-		  data: {
-			codigoPix: '987678954',
-		  },
-		};
-	
-		axios.post.mockImplementation(() => Promise.resolve(mockResponse));
-	
-		const pedidoProps: PedidoProps = {
-		  produtos: ['1'],
-		  cliente: 'Cliente 1',
-		  valorTotal: 0,
-		  numeroPedido: 0,
-		  statusPagamento: StatusPagamentoEnum.PENDENTE,
-		  statusPedido: StatusPedidoEnum.RECEBIDO,
-		};
-	
-		const novoPagamento = await PedidoUseCases.CriarPagamento(pedidoProps);
+    expect(pedidoRepository.EditarPedido).toHaveBeenCalledWith(
+      expect.objectContaining({
+        id: '1',
+        statusPagamento: StatusPagamentoEnum.APROVADO,
+      })
+    );
 
-		expect(novoPagamento).toBeDefined();
-	  });
+    expect(updatedPedido).toBeDefined();
+    expect(updatedPedido.statusPagamento).toBe(statusPagamento);
 
-	  test('Deve alterar o status do pagamento do pedido', async () => {
-		const pedidoEncontradoMock: PedidoOutput = {
-		  id: '1',
-		  statusPagamento: StatusPagamentoEnum.PENDENTE,
-		  statusPedido: StatusPedidoEnum.RECEBIDO,
-		};
-
-		const codigoPagamento = '123456789';
-		const statusPagamento = StatusPagamentoEnum.APROVADO;
-
-		pedidoRepository.BuscarPedidoPorCodigoPagamento = jest.fn().mockResolvedValue(pedidoEncontradoMock);
-	
-		const updatedPedido = await PedidoUseCases.AlterarStatusPagamentoPedido(
-		  pedidoRepository,
-		  codigoPagamento,
-		  statusPagamento
-		);
-	
-		pedidoRepository.EditarPedido = jest.fn();
-
-		expect(pedidoRepository.BuscarPedidoPorCodigoPagamento).toHaveBeenCalledWith(codigoPagamento);
-		expect(pedidoRepository.EditarPedido).toHaveBeenCalledWith(expect.any(Object));
-	
-		expect(updatedPedido).toBeDefined();
-		expect(updatedPedido.statusPagamento).toBe(statusPagamento);
-	
-		if (statusPagamento === StatusPagamentoEnum.NEGADO) {
-		  expect(updatedPedido.statusPedido).toBe(StatusPedidoEnum.CANCELADO);
-		} else if (statusPagamento === StatusPagamentoEnum.APROVADO) {
-		  expect(updatedPedido.statusPedido).toBe(StatusPedidoEnum.PREPARACAO);
-		}
-	  });
-
+    if (statusPagamento === StatusPagamentoEnum.NEGADO) {
+      expect(updatedPedido.statusPedido).toBe(StatusPedidoEnum.CANCELADO);
+    } else if (statusPagamento === StatusPagamentoEnum.APROVADO) {
+      expect(updatedPedido.statusPedido).toBe(StatusPedidoEnum.PREPARACAO);
+    }
+  });
 });
