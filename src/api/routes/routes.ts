2ce35838e346948de8248c2d9f07f10c1522b680
fanticheli <igor.fanticheli@hotmail.com>
2024-01-26 19:51:37 -0300

    feature: implements port 80

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-	res.send("MicroService Pedido is healthy!");
+	res.send("MicroService Pedido is healthy!!!!");
 });
 
 module.exports = router;
